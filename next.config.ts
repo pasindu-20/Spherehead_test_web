434565bfa032f4519264a9f58699740fecb33864
yasurirashmika <iyasuri.rash@gmail.com>
2026-06-03 13:20:10 +0530

    build update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   devIndicators: false,
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
